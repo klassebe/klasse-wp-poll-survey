84792d2dd0ea745a0a0b2705e541737b3defdcca
koenHuybrechts <koen@appsaloon.be>
2014-03-07 16:13:29 +0100

    Group unit testing in folder

--- klasse-wp-poll-survey.php
+++ klasse-wp-poll-survey.php
@@ -44,21 +44,10 @@ require_once( plugin_dir_path( __FILE__ ) . 'public/class-klass-wp-poll-survey.p
 /*
  * Register hooks that are fired when the plugin is activated or deactivated.
  * When the plugin is deleted, the uninstall.php file is loaded.
- *
- * @TODO:
- *
- * - replace Plugin_Name with the name of the class defined in
- *   `class-plugin-name.php`
  */
 register_activation_hook( __FILE__, array( 'Klasse_WP_Poll_Survey', 'activate' ) );
 register_deactivation_hook( __FILE__, array( 'Klasse_WP_Poll_Survey', 'deactivate' ) );
 
-/*
- * @TODO:
- *
- * - replace Plugin_Name with the name of the class defined in
- *   `class-plugin-name.php`
- */
 add_action( 'plugins_loaded', array( 'Klasse_WP_Poll_Survey', 'get_instance' ) );
 
 /*----------------------------------------------------------------------------*
---
+++ tests/unit/InstallTest.php
@@ -0,0 +1,7 @@
+<?php
+
+class InstallTest extends WP_UnitTestCase {
+    function testCrazyThing() {
+        $this->assertTrue(true);
+    }
+}
