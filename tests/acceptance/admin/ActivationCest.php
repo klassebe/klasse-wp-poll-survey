131a16e0d247903dd364dd5959a3068e29c6e419
koenHuybrechts <koen@appsaloon.be>
2014-03-20 16:10:38 +0100

    Centralize login functions

--- tests/acceptance/admin/ActivationCest.php
+++ tests/acceptance/admin/ActivationCest.php
@@ -12,31 +12,18 @@ class ActivationCest
     public function _after()
     {
     }
-
-    protected function login(WebGuy $I)
-    {
-        $I->amOnPage('/wp-admin');
-        $I->fillField('Username', 'admin');
-        $I->fillField('Password','');
-        $I->click('Log In');
-    }
-
-    /**
-     * @before login
-     */
     function activatePlugin(WebGuy $I)
     {
+        AdminCommons::logMeIn($I);
         $I->wantTo('activate the plugin');
         $I->click('Plugins');
         $I->click('#klasse-wordpress-poll-survey span.activate a');
         $I->see('Deactivate');
     }
 
-    /**
-     * @before login
-     */
     function deactivatePlugin(WebGuy $I)
     {
+        AdminCommons::logMeIn($I);
         $I->wantTo('de-activate the plugin');
         $I->click('Plugins');
         $I->click('#klasse-wordpress-poll-survey span.deactivate a');
