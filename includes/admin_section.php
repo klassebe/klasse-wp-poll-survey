cb553d13292ba04619dd71b291185a226707770e
koenHuybrechts <koen@appsaloon.be>
2014-03-26 12:52:32 +0100

    Merge and remove legacy code

--- includes/admin_section.php
+++ includes/admin_section.php
@@ -5,30 +5,10 @@ require_once __DIR__ . '/poll_list_table.php';
 
 
 class admin_section {
-    public static function display_tests_temp(){
-        $arguments = array(
-            'post_type' => 'kwps_poll',
-//            'post_parent' => 1,
-        );
+    public static function display_form()
+    {
+        include_once dirname(__FILE__) . '/../views/add.php';
 
-        $tests_as_arrays = array();
-
-        $tests = get_posts($arguments);
-        foreach($tests as $test){
-            $meta_keys = get_post_custom_keys($test->ID);
-//            var_dump($meta_data); die;
-            foreach($meta_keys as $key){
-                $meta_data = get_post_meta($test->ID, $key, true);
-                $test->$key = $meta_data;
-            }
-
-            array_push($tests_as_arrays, (array) $test);
-        }
-
-        $json_output = json_encode($tests_as_arrays);
-//        wp_send_json($json_output);
-
-        var_dump($tests_as_arrays); die;
     }
 
     public static function display_tests() {
