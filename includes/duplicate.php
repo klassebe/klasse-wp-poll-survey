3a11c764daff8edde43ced95d7d888ab8b027c7e
Koen Gabriëls <koeng@appsaloon.be>
2014-05-13 15:55:21 +0200

    Adds locked post_status and superclass kwps_post_status

--- includes/duplicate.php
+++ includes/duplicate.php
@@ -2,25 +2,13 @@
 
 namespace includes;
 
-    class Duplicate {
+require_once 'kwps_post_status.php';
 
-        public static function register_post_status(){
-            register_post_status( 'duplicate', array(
-                'label'                     => _x( 'Duplicate', 'kwps_test_modus' ),
-                'public'                    => true,
-                'exclude_from_search'       => false,
-                'show_in_admin_all_list'    => true,
-                'show_in_admin_status_list' => true,
-                'label_count'               => _n_noop( 'Duplicate <span class="count">(%s)</span>', 'Duplicate <span class="count">(%s)</span>' ),
-            ) );
-        }
+    class Duplicate extends Kwps_Post_Status{
+        public static $post_status = 'duplicate';
 
-        public static function display_post_status($states){
-            global $post;
+        public static $label_singular ='Duplicate';
+        public static $label_plural ='Duplicates';
 
-            if( 'kwps_test_modus' == $post->post_type && 'duplicate' == $post->post_status ){
-                $states[] = __('Duplicate');
-            }
-            return $states;
-        }
+        public static $available_post_types = array('kwps_test_modus');
     }
