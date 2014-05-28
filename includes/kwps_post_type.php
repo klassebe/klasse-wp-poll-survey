67dca26909d86fd0224c5a0c4226ed2516245c28
koenHuybrechts <koen@appsaloon.be>
2014-05-28 11:50:21 +0200

    Don't send success status

--- includes/kwps_post_type.php
+++ includes/kwps_post_type.php
@@ -148,7 +148,7 @@ abstract class Kwps_Post_Type implements \includes\Post_Type_Interface {
             wp_send_json_error($errors);
         } else {
             $post = static::save_post($request_data);
-            wp_send_json_success( $post );
+            wp_send_json( $post );
         }
 
         die();
