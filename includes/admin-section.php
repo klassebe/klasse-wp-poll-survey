948833698030c43ea477ed5b825891138f484d00
Koen Gabriëls <koeng@appsaloon.be>
2014-07-02 10:22:49 +0200

    adds test_collection_outro to list of javascript objects for admin section frontend

--- includes/admin-section.php
+++ includes/admin-section.php
@@ -148,8 +148,9 @@ class admin_section {
                     echo 'post not of type kwps_test_collection';
                 } else {
                     $test_collection = Test_Collection::get_as_array($current_post->ID);
+                    $test_collection_outro = Test_Collection_Outro::get_one_by_post_parent($current_post->ID);
 
-                    $tests = array($test_collection);
+                    $tests = array($test_collection, $test_collection_outro);
 
                     $versions = Version::get_all_by_post_parent($current_post->ID);
 
