dbc8b59098764ffca05621e3f5df15ed8c808e94
Koen Gabriëls <koeng@appsaloon.be>
2014-07-07 15:36:11 +0200

    Removes debugging code

--- klasse-wp-poll-survey.php
+++ klasse-wp-poll-survey.php
@@ -75,18 +75,7 @@ function debug_settings(){
 }
 
 include_once 'add-session.php';
-require_once __DIR__ . '/includes/charts/grouped-bar-chart.php';
-add_shortcode('kwps_get_test_result', 'get_result_profile' );
 
-function get_result_profile($atts){
-    extract( shortcode_atts( array(
-        'test_collection_id' => 0,
-    ), $atts ) );
-
-    $output = \includes\Grouped_Bar_Chart::get_chart_per_profile($test_collection_id, null);
-    var_dump($output); die;
-
-}
 
 add_filter( 'display_post_states', array('\includes\duplicate','display_post_status'), 10,2);
 
