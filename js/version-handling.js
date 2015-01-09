00fab0e3f48339baf1a2c84ac69ff266ea52e7e4
Jo Jordens <appsaloonmacpro17@MacBook-Pro-van-appsaloonMacPro17.local>
2015-01-09 09:07:28 +0100

    #116 add result button done

--- js/version-handling.js
+++ js/version-handling.js
@@ -15,6 +15,73 @@ jQuery(document).ready(function($) {
   $(document).on('click','.kwps-content-edit', showEditor);
   $(document).on('click','.kwps-content-editor-save', updateValues);
 
+
+  var button = $('#kwps-add-result-button').detach();
+  $('#wp-outro-media-buttons').after(button);
+
+	$('#kwps-add-result-button').on('click', function() {
+		var output ='';
+		var allowedTypes;
+
+		if(testModus.post_type === 'kwps_coll_outro') {
+			allowedTypes = testModus._kwps_allowed_output_types_test_collection;
+		} else {
+			allowedTypes = testModus._kwps_allowed_output_types;
+		}
+
+		tb_show('','../../wp-content/plugins/klasse-wp-poll-survey/classes/show-charts.php?type=image&amp;TB_iframe=true');
+
+		var request = $.ajax({
+										url: 'admin-ajax.php?action=kwps_get_result_page',
+										context: document.body
+									});
+		request.done(function(request, status, error) {
+				$('iframe').contents().find('#kwps-result-page').append(request);
+		})
+		request.fail(function() {
+				alert(kwps_translations['Errors occurred. Please check below for more information.']);
+		});
+
+		$.each(allowedTypes, function (key, value) {
+			output +=	 '<div id="' + value + '" class="media-item left"><label><h4>' + value.charAt(0).toUpperCase() + value.slice(1).split('-').join(' ') + '</h4><input type="radio" name="results" value="' + value + '"><img class="thumbnail" src="images/' + value + '.png" alt="' + value + '" height="128" width="128"></label></div>';
+		});
+
+		var selectedResult;
+		// Check the iframe if the content is already loaded
+		var timer = setInterval( function () {
+
+      findInIFrame('#charts').append(output);
+
+      findInIFrame('input:radio').hide();
+			findInIFrame('input:radio').on('click', function () {
+        findInIFrame('.selected').removeClass();
+        $(this).next().addClass('selected');
+				selectedResult = $(this).next().attr('alt');
+			});
+			findInIFrame('#add-result-to-editor').on('click', function () {
+				if (selectedResult) {
+					findInIFrame('#tinymce').append('[kwps_result result='+ selectedResult + ']');
+          var textarea = $('#wp-outro-editor-container textarea');
+          var newText = textarea.text() + '[kwps_result result=' + selectedResult + ']';
+          textarea.text(newText);
+					tb_remove();
+				} else {
+					alert('Please select a result view to import');
+				}
+			});
+			if (findInIFrame('#charts').length > 0) {
+				clearInterval(timer);
+			}
+		}, 100);
+
+			return false;
+	});
+
+  var findInIFrame = function (element) {
+    return $('iframe').contents().find(element);
+  }
+
+
   var openCollapse = function () {
     var objectName = $('#kwps-version');
     objectName = 'collapseStatusVersionID' + objectName.find('input[name="ID"]').attr('value') + 'post_parent' + objectName.find('input[name="post_parent"]').attr('value');
