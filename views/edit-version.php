cb8115a5223cb3e1492f9b46d299cc0f8ec4c76f
Koen Gabriëls <koeng@appsaloon.be>
2015-01-22 08:12:43 +0100

    Adds add result button for intro result

--- views/edit-version.php
+++ views/edit-version.php
@@ -144,6 +144,7 @@ $test_collection_url = get_admin_url() .'/admin.php?page=klasse-wp-poll-survey_e
                 <?php if( isset( $intro_result['ID'] ) ): ?>
                     <input type="hidden" name="ID" value="<?php echo $intro_result['ID'];?>" class="kwps-single_input">
                 <?php endif;?>
+                <a class="button" id="kwps-add-intro-result-button">Add result</a>
                 <input type="hidden" name="post_status" value="<?php if( isset( $intro_result['post_status'] ) ) echo $intro_result['post_status'];?>" class="kwps-single_input">
                 <textarea style="display: none" name="post_content" class="kwps-single_input"><?php echo (isset($intro_result['post_content']))? $intro_result['post_content'] : "Intro Result" ?></textarea>
 
