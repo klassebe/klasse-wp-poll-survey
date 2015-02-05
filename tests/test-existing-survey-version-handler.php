07b71b88ddee302c2c2440ae84bf746dcbe96101
Koen Gabriëls <koeng@appsaloon.be>
2015-02-05 16:45:47 +0100

    removes multiple answer options instead of 1 in test, renames save-trashed-answer-option-test to save-trashed-answer-options

--- tests/test-existing-survey-version-handler.php
+++ tests/test-existing-survey-version-handler.php
@@ -10,26 +10,26 @@ class Existing_Survey_Version_Handler_Test extends Kwps_Test {
         $this->test_data_folder = __DIR__ . '/../form-test-data/existing-version/survey/';
     }
 
-//    function test_validate_remove_answer_option(){
-//        $this->checkOutPutWithFormTestData( 'removing-answer-option-test.php' );
-//    }
+    function test_validate_remove_answer_option(){
+        $this->checkOutPutWithFormTestData( 'removing-answer-option-test.php' );
+    }
 
-//    function test_validate_remove_too_many_answer_options() {
-//        $this->checkOutPutWithFormTestData( 'removing-too-many-answer-options-test.php' );
-//    }
-//
-//    function test_validate_remove_too_many_questions() {
-//        $this->checkOutPutWithFormTestData( 'removing-too-many-questions-test.php' );
-//    }
-//
-//    function test_validate_remove_too_many_question_groups() {
-//        $this->checkOutPutWithFormTestData( 'removing-too-many-question-groups-test.php');
-//    }
-//
-//    function test_save_trashed_answer_option() {
-//        $this->check_saved_and_updated_siblings( 'save-trashed-answer-option-test.php' );
-//    }
-//
+    function test_validate_remove_too_many_answer_options() {
+        $this->checkOutPutWithFormTestData( 'removing-too-many-answer-options-test.php' );
+    }
+
+    function test_validate_remove_too_many_questions() {
+        $this->checkOutPutWithFormTestData( 'removing-too-many-questions-test.php' );
+    }
+
+    function test_validate_remove_too_many_question_groups() {
+        $this->checkOutPutWithFormTestData( 'removing-too-many-question-groups-test.php');
+    }
+
+    function test_save_trashed_answer_option() {
+        $this->check_saved_and_updated_siblings( 'save-trashed-answer-options-test.php' );
+    }
+
     function test_save_added_answer_option() {
         $this->check_saved_and_updated_siblings( 'save-added-answer-option-test.php');
     }
