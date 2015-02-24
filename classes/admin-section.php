1a402c8aaa284f8bb2ead092df3eb626659fd316
Koen Gabriëls <koeng@appsaloon.be>
2015-02-24 10:21:16 +0100

    Uses the build java scripts from the assets folder, adds built js files

--- classes/admin-section.php
+++ classes/admin-section.php
@@ -132,14 +132,14 @@ class admin_section {
             wp_enqueue_script( 'jquery-ui-core' );
 
             wp_register_script('klasse_wp_poll_survey_plugin_admin_ays', plugins_url('../js/bower_components/jquery.are-you-sure/jquery.are-you-sure.js', __FILE__));
-            wp_register_script('klasse_wp_poll_survey_plugin_admin_scripts', plugins_url('../js/version-handling.js', __FILE__));
+            wp_register_script('klasse_wp_poll_survey_plugin_admin_scripts', plugins_url('../assets/js/kwps_admin-versions.js', __FILE__));
             wp_localize_script('klasse_wp_poll_survey_plugin_admin_scripts', 'WPURLS', array( 'siteurl' => get_option('siteurl') ));
 
             wp_enqueue_script('klasse_wp_poll_survey_plugin_admin_ays');
             wp_enqueue_script('klasse_wp_poll_survey_plugin_admin_scripts');
         } elseif( isset( $_REQUEST['section'] ) && isset( $_REQUEST['tab'] ) && 'edit_test_collection' == $_REQUEST['section'] && 'results' == $_REQUEST['tab'] ) {
             $testmodus = Test_Collection::get_test_modus( $_REQUEST['id']);
-            wp_register_script('klasse_wp_poll_survey_plugin_admin_results_scripts', plugins_url('../js/admin-results.js', __FILE__));
+            wp_register_script('klasse_wp_poll_survey_plugin_admin_results_scripts', plugins_url('../assets/js/kwps_admin-results.js', __FILE__));
             wp_register_script('klasse_wp_poll_survey_plugin_admin_results_highcharts', plugins_url('../js/bower_components/highcharts-release/highcharts.js', __FILE__));
             wp_enqueue_script( 'klasse_wp_poll_survey_plugin_admin_results_scripts');
             wp_enqueue_script( 'klasse_wp_poll_survey_plugin_admin_results_highcharts');
