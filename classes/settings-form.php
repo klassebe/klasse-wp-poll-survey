f192388bf30f131ea3c0b3de8e5999c0a697086c
Koen Gabriëls <koeng@appsaloon.be>
2015-02-26 16:49:44 +0100

    Adds Settings_Form and Test_Settings_Form class

---
+++ classes/settings-form.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: koengabriels
+ * Date: 26/02/15
+ * Time: 16:30
+ */
+
+namespace kwps_classes;
+
+
+class Settings_Form {
+
+    protected $data;
+
+    public function __construct( $data ) {
+        $this->data = $data;
+    }
+
+    public function validate() {
+
+    }
+} 
---
+++ tests/test-settings-form.php
@@ -0,0 +1,5 @@
+<?php
+
+class Test_Settings_Form {
+
+} 
